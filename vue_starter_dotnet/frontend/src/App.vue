8d55e73635c324d9a7ea155c6fe57f8ee01fb3ac
Christina Rivera <cnrivera@gmail.com>
2020-04-07 10:16:32 -0400

    Updates to front end navigation

--- vue_starter_dotnet/frontend/src/App.vue
+++ vue_starter_dotnet/frontend/src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/standardcakes">Standard Cakes</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg">
+    <div class="justify-content-start" id="header-img">
+      <img class="header-img" src="./assets/elevate-sweets.png">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><router-link to="/">Home</router-link></li>
+        <li class="nav-item"><router-link to="/standardcakes">Standard Cakes</router-link></li>
+      </ul>
+    </div>  
     <router-view/>
+    </nav>
   </div>
 </template>
 
 <style>
 body{
   margin: 100px;
+}
+
+#header-img {
+  height: 150px;
+}
+
+#header-img > img {
+  height: 100%;
 }
 </style>
