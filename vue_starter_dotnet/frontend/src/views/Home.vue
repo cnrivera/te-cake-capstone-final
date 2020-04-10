cf487e3e5db186d48252b227f610f24b2b3a0df3
XanthHively <xanthhive@gmail.com>
2020-04-10 16:43:32 -0400

    home page pretty

--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
 <div id="homepage">
-  <div id="hometext">
-    <p>Elevate Sweets provides delicious artisan cakes and treats for all occasions using the finest seasonal ingredients available. All of our baked goods are made fresh daily by veteran bakers with a keen sense for flavor and aesthetic. Select from one of our house favorites or create a custom cake from our currently available options.</p>
-  </div>
-  <div id="homebtn">
-    <router-link :to="{name: 'standard-cakes'}" class="btn btn-info" role="button">House Favorites</router-link>
-    <router-link :to="{name: ''}" class="btn btn-info" role="button">Custom Cake</router-link>
+  <div id="homeinfo">
+    <div class="hometext">
+      <p>Elevate Sweets provides delicious artisan cakes and treats for all occasions using the finest seasonal ingredients available. All of our baked goods are made fresh daily by veteran bakers with a keen sense for flavor and aesthetic. Select from one of our house favorites or create a custom cake from our currently available options.</p>
+    </div>
+    <div class="homebtn">
+      <router-link :to="{name: 'standard-cakes'}" class="btn btn-info" role="button">House Favorites</router-link>
+      <router-link :to="{name: ''}" class="btn btn-info" role="button">Custom Cake</router-link>
+    </div>
   </div>
 </div>
 </template>
@@ -21,23 +23,45 @@ export default {
   background-image: url('../../src/assets/home-background.jpg');
   height: 85vh;
   background-position: center;
-  background-repeat: no-repeat;
   background-size: cover;
-}
-#hometext {
-  width: 55%;
-  padding: 10vh;
-  font-size: 1.1vw;
-  font-weight: bold;
-}
-#homebtn {
   padding: 10vh;
 }
-#homebtn .btn {
+.hometext {
+  width: 100%;
+  padding: 2vh;
+  background-color: hsla(187, 92%, 91%, 0.6);
+  border: 4px solid rgba(68, 68, 68, 0.5);
+  border-radius: 10px;
+  font-size: 22px;
+}
+.homebtn {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+#homeinfo {
+  width: 52%;
+  display: flex;
+  flex-wrap: wrap;
+}
+#homeinfo .btn {
   border: 2px solid black;
   box-shadow: 1px 2px 40px rgba(0,0,0,.6);
   font-size: 2.2vw;
   font-weight: bold;
-  margin-left: 4vh;
+  margin: 2vh;
+}
+@media (max-width:900px) {
+  #homepage {
+    background-position: right;
+    background-color: hsla(188, 56%, 8%, 0.5);
+    background-blend-mode: multiply;
+  }
+  #homeinfo {
+    width: 100%;
+  }
+  .homebtn .btn {
+    font-size: 5vw;
+  }
 }
 </style>
