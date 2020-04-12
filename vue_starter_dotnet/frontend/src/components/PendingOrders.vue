98ca1aa135ec0ff2dd998bdbde1b26ad59bc51b8
Kim Westendorf <ktwestendorf@gmail.com>
2020-04-12 13:06:22 -0400

    changes to pending orders page to add dropdown for status

--- vue_starter_dotnet/frontend/src/components/PendingOrders.vue
+++ vue_starter_dotnet/frontend/src/components/PendingOrders.vue
@@ -3,12 +3,19 @@
          <h1>All Pending Orders</h1>    
       <div v-for="order in orders" :key="order.name" class="orderdetails">
       <ul>
+        <li><b>Order Number:</b> {{order.orderId}}
         <li><b>Customer Name:</b> {{order.customerName}}</li>
         <li><b>Pick up Date and Time:</b> {{order.date}} {{order.time}}</li>
-        <li><b>Pick up Date and Time:</b> {{order.pickupDateTime}}</li>
-        
+        <li><b>Current Order Status:</b>{{order.orderStatus}}</li>
+        <li><label for="size"><b>Change Order Status To:</b></label>
+      <select v-model="order.orderStatus">
+      <option v-for="status in statusChange" :key="status" :value="status">
+       {{ status }} 
+             </option>
+</select>
+</li>
       </ul>
-      </div>
+            </div>
       
   </div>
 </template>
@@ -19,6 +26,12 @@ export default {
     data() {
       return {
         orders: [],
+        statusChange: [
+          'pending',
+          'canceled',
+          'ready',
+          'completed'
+        ]
       }
     },
     methods: {
